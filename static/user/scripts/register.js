296fbe17f227f5dc83b75f4e4b54f5cc1a82d121
XufengCh <xfch293@gmail.com>
2019-08-13 16:08:28 +0800

    Finish LOGIN

--- static/user/scripts/register.js
+++ static/user/scripts/register.js
@@ -5,12 +5,6 @@ var registerForm = new Vue({
         nickname : '',
         password1 : '',
         password2 : '',
-        //usernameHelptext : "请输入您的电子邮件地址作为用户名，长度需小于150字符。",
-        //passwordHelptext : "请输入长度在8 ~ 30位之间的密码，可包含特殊字符。",
-        //usernameError : "该邮箱已被注册！",
-        //password1Error : "密码长度不符合要求！",
-        //password2Error : "两次输入密码不一致！",
-        //username_is_valid : true,
         password1IsValid : true,
         password2IsValid : true
     },
